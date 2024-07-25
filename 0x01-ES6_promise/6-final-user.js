08e755067fcc19bdd3d28a390390c84079fc7b99
OMARTK1 <elyazidi.omar@gmail.com>
2024-07-25 03:20:27 +0100

    check 6 is fine

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,23 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
+async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => 
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }))
-  );
+    const [userResult, photoResult] = await Promise.all([userPromise, photoPromise]);
+
+    const resultsArr = [
+      { status: 'resolved', value: userResult },
+      { status: 'resolved', value: photoResult },
+    ];
+
+    return resultsArr;
+  } catch (error) {
+    console.error('Error in handleProfileSignup:', error);
+    throw error;
+  }
 }
+
+export default handleProfileSignup;
