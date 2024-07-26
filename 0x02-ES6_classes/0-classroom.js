0797be35f01297e5675a7837c8908529cef62028
OMARTK1 <elyazidi.omar@gmail.com>
2024-07-26 02:54:16 +0100

    all done

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+export default class ClassRoom {
+// class accept one attribute named maxStudentsSize (Number) and assigned to _maxStudentsSize
+
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
