512edbfb3aedca7f96b99a2df3d38038c2f4458e
OMARTK1 <elyazidi.omar@gmail.com>
2024-07-26 03:00:13 +0100

    all done

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
   const classarr = [];
-  
+
   for (const size of sizes) {
     classarr.push(new ClassRoom(size));
   }
